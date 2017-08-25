5f6e52f98b0ed409662a669c00fe7318c740c23c
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-08-25 12:06:58 +0300

    more general script for cmake-js

--- conan_package/base.py
+++ conan_package/base.py
@@ -65,7 +65,7 @@ class LibtorrentNodeBase(ConanFile):
         # We run cmake-js compile step directly, skipping node-gyp build system because we want to use the
         # conan dependencies of the project consuming the conan package. node-gyp build system is only used
         # when libtorrent-node is being built as a standalone npm package
-        self.run('npm run compile -- %s %s %s %s %s' % (debug, arch, runtime, runtime_version, msvc_runtime))
+        self.run('npm run cmake-js -- compile %s %s %s %s %s' % (debug, arch, runtime, runtime_version, msvc_runtime))
 
     def package(self):
         self.copy("*.hpp", dst="include/libtorrent-node/", src="repo/module/src/")
